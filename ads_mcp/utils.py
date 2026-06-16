22a71bf1fe7b515d27a53eb7cb5c5a326f1b2df0
defibalista <196204456+defibalista@users.noreply.github.com>
2026-06-16 16:42:41 +0300

    fix: use refresh token credentials instead of ADC

--- ads_mcp/utils.py
+++ ads_mcp/utils.py
@@ -65,14 +65,25 @@ def prevent_stdio_inheritance():
 
 
 def _create_credentials() -> google.auth.credentials.Credentials:
-    """Returns Application Default Credentials with the Google Ads scope, or the FastMCP token if found."""
-    from fastmcp.server.dependencies import get_access_token
+    """Returns credentials using refresh token from env vars, falling back to ADC."""
     from google.oauth2.credentials import Credentials
+    from google.auth.transport.requests import Request
 
-    token_obj = get_access_token()
-    if token_obj and token_obj.token:
-        # Create credentials using the access token provided by FastMCP
-        return Credentials(token=token_obj.token)
+    client_id = os.environ.get("GOOGLE_ADS_CLIENT_ID")
+    client_secret = os.environ.get("GOOGLE_ADS_CLIENT_SECRET")
+    refresh_token = os.environ.get("GOOGLE_ADS_REFRESH_TOKEN")
+
+    if client_id and client_secret and refresh_token:
+        creds = Credentials(
+            token=None,
+            refresh_token=refresh_token,
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=client_id,
+            client_secret=client_secret,
+            scopes=[_ADS_SCOPE],
+        )
+        creds.refresh(Request())
+        return creds
 
     with prevent_stdio_inheritance():
         credentials, _ = google.auth.default(scopes=[_ADS_SCOPE])
